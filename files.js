3b854dfb2598bb4e34af2bafa8c15d0ffff0a78f
Noel Thomson <noel.thomson@gmail.com>
2020-12-11 00:32:13 -0500

    first commit

--- files.js
+++ files.js
@@ -4,31 +4,29 @@ const fs = require('fs');
 fs.readFile('./docs/blog.txt', (err, data) => {
   if (err) {
     console.log(err);
-  }  
+  }
   console.log(data.toString());
 });
-
-// console.log('last line');
 
 // writing files
 fs.writeFile('./docs/blog.txt', 'hello, world', () => {
   console.log('file was written');
 });
 
-fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
-  console.log('file was written');
+fs.writeFile('./docs/blog.txt', 'hello, again', () => {
+  console.log('file was overwritten');
 });
 
 // directories
 if (!fs.existsSync('./assets')) {
-  fs.mkdir('./assets', err => {
+  fs.mkdir('./assets', (err) => {
     if (err) {
       console.log(err);
     }
     console.log('folder created');
   });
 } else {
-  fs.rmdir('./assets', err => {
+  fs.rmdir('./assets', (err) => {
     if (err) {
       console.log(err);
     }
@@ -37,8 +35,8 @@ if (!fs.existsSync('./assets')) {
 }
 
 // deleting files
-if (fs.existsSync('./docs/deleteme.txt')) {
-  fs.unlink('./docs/deleteme.txt', err => {
+if (fs.existsSync('./docs/delete-me.txt')) {
+  fs.unlink('./docs/delete-me.txt', (err) => {
     if (err) {
       console.log(err);
     }
