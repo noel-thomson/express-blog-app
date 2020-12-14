aceb9b8f5965935e64efc64667dcd647ad5bba9f
Noel Thomson <noel.thomson@gmail.com>
2020-12-14 16:02:39 -0500

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb <user>:<password> ... /<db-name>
+const dbURI =
+  'mongodb+srv://johnsmith:mongo1234@cluster0.p9iyl.mongodb.net/blogs-db?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('connected to DB');
+    // listen for requests
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -35,9 +45,18 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
   ];
   res.render('index', { title: 'Home', blogs });
 });
